7544057f1e9149082662a59f4e3c136e5a1b6370
Cyrus <cyrus@lambdacore.de>
2024-08-12 20:49:01 +0200

    Revert "Dev cyrus (#1)"
    This reverts commit e2adf87c2678c4b0ade2e09332efa6f0d20054a7.

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 import asyncio
-import logging
 import threading
-import os
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import ThreadingOSCUDPServer
-from pythonosc import udp_client
 from tinyoscquery.queryservice import OSCQueryService
 from tinyoscquery.utility import get_open_tcp_port, get_open_udp_port
-from flask import Flask
-from flask_restful import Api, Resource
-from waitress import serve
 from vrphone import VRPhone
 from config import Config
 from gui import Gui
+import os
 
-
-class ReceiveCallback(Resource):
-    def get(self, command, caller):
-        vrphone.microsip_callback(command=command, caller=caller)
-        return
-
-    def put(self, command, caller):
-        vrphone.microsip_callback(command=command, caller=caller)
-        return
 
 def start_oscquery(server_udp_port, server_tcp_port):
     def start_server():
         oscquery_server = OSCQueryService("VRC VR Phone", server_tcp_port, server_udp_port)
         oscquery_server.advertise_endpoint("/avatar")
     return start_server
-
-def start_callbackapi():
-    serve(app, host="127.0.0.1", port=19001)
-    return
 
 gui = None
 try:
@@ -42,39 +24,27 @@ try:
     gui = Gui(config=cfg, window_width=550,
               window_height=1000, logo_path=logo_path)
     gui.init()
-    osc_client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
-    vrphone = VRPhone(config=cfg, gui=gui, osc_client=osc_client)
+    vrphone = VRPhone(config=cfg, gui=gui)
+    vrphone.init()
     dispatcher = Dispatcher()
     vrphone.map_parameters(dispatcher)
 
-    #Callback API Init
-    app = Flask(__name__)
-    app.logger.setLevel(logging.ERROR)
-    api = Api(app=app)
-    api.add_resource(ReceiveCallback, '/<string:command>/<string:caller>')
-    
     server_udp_port = cfg.get_by_key("server_port")
 
-    #Start OSCQuery if active which chooses dynamic server ports for us
     if cfg.get_by_key("use_oscquery"):
         server_udp_port = get_open_udp_port()
         server_tcp_port = get_open_tcp_port()
         threading.Thread(target=start_oscquery(server_udp_port, server_tcp_port),
                          daemon=True).start()
-        gui.print_terminal("OSC query started, advertising ports UDP: {} TCP: {}".format(server_udp_port, server_tcp_port))
+        gui.print_terminal("OSC query started\nAdvertising UDP Port: {} TCP Port: {} to VRC".format(server_udp_port, server_tcp_port))
 
     osc_server = ThreadingOSCUDPServer(
         ("127.0.0.1", server_udp_port), dispatcher, asyncio.new_event_loop())
-    gui.print_terminal("OSC server starting on port: {}".format(server_udp_port))
-
-    #Start Server Threads for VRPhone
-    #Start OSC ServerThread
+    gui.print_terminal("OSC server started on port: {}".format(server_udp_port))
     threading.Thread(target=lambda: osc_server.serve_forever(2),
                      daemon=True).start()
-    #Start microsip HTTP callback API
-    threading.Thread(target=start_callbackapi,
+    threading.Thread(target=vrphone.watch,
                      daemon=True).start()
-    vrphone.run()
     gui.run()
 except KeyboardInterrupt:
     print("Shutting Down...\n")
