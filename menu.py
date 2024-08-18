f8f640133848e464cd9427affb4e89ea2ed50045
Cyrus <cyrus@lambdacore.de>
2024-08-18 14:23:02 +0200

    Don't drop  callbacks, they need to be handled

--- menu.py
+++ menu.py
@@ -123,10 +123,6 @@ class Menu:
             return
             
     def handle_callback_input(self, command, caller):
-        #Loading transition mode, we don't accept input here
-        if self.active_mode == 2:
-            self.gui.print_terminal("log_verbose: Drop callback command: {} active_mode: {}".format(command, self.active_mode)) if self.config.get_by_key("log_verbose") else None
-            return
         self.gui.print_terminal("log_verbose: Handle callback command: {} active_mode: {}".format(command, self.active_mode)) if self.config.get_by_key("log_verbose") else None
         match command:
             case "cmdCallEnd" | "cmdCallBusy":
